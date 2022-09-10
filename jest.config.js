c1995a4f55763ac33bc40fab3ab277e611db2272
Dieter Oberkofler <1009585+doberkofler@users.noreply.github.com>
2022-09-10 22:50:15 +0200

    Updated all dependencies

--- jest.config.js
+++ jest.config.js
@@ -67,13 +67,6 @@ module.exports = {
 
   // A set of global variables that need to be available in all test environments
   // globals: {},
-	globals: {
-		'ts-jest': {
-			tsconfig: 'tsconfig.src.json',
-			diagnostics: true,
-			useESM: true,
-		}
-	},
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -183,6 +176,16 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   // transform: undefined,
+	transform: {
+		'\\.[jt]sx?$': [
+			'ts-jest',
+			{
+				tsconfig: 'tsconfig.src.json',
+				diagnostics: true,
+				useESM: true,
+			},
+		],
+	},
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
