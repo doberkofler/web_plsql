e0dc31a6e274762f55479d553de49c262b6b6bd8
Dieter Oberkofler <1009585+doberkofler@users.noreply.github.com>
2025-02-05 21:41:15 +0100

    Cleanup

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,8 @@
 export { startServer } from "./server.js";
 export * from "./shutdown.js";
-export * from "./oracle";
-export * from "./util";
-export * from "./version";
+export * from "./oracle.js";
+export * from "./util.js";
+export * from "./version.js";
 export { handlerWebPlSql } from "./handlerPlSql.js";
 export { handlerLogger } from "./handlerLogger.js";
 export { handlerUpload } from "./handlerUpload.js";
