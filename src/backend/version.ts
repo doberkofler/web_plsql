4cd0fdbf3cfacfc7ddccaa1f135c70a1a5d4e373
Dieter Oberkofler <1009585+doberkofler@users.noreply.github.com>
2026-03-28 23:37:19 +0100

    chore: update typescript

--- src/backend/version.ts
+++ src/backend/version.ts
@@ -8,6 +8,6 @@ declare const __VERSION__: string;
 
 /**
  * Returns the current library version
- * @returns {string} - Version.
+ * @returns Version.
  */
 export const getVersion = () => __VERSION__;
