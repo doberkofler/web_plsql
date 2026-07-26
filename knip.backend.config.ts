248f133ba7ee4da6bbac207f1c463b38e663d71a
Dieter Oberkofler <dieter.oberkofler@gmail.com>
2026-07-26 10:07:34 +0200

    chore: update dependencies

--- knip.backend.config.ts
+++ knip.backend.config.ts
@@ -4,6 +4,7 @@ const config: KnipConfig = {
 	$schema: 'https://unpkg.com/knip@5/schema.json',
 	entry: ['src/backend/server/server.ts'],
 	project: ['src/backend/**/*.ts'],
+	tailwind: false,
 	ignore: [],
 	ignoreDependencies: ['chart.js', 'tailwindcss', 'conventional-changelog-angular'],
 };
--- knip.frontend.config.ts
+++ knip.frontend.config.ts
@@ -3,9 +3,10 @@ import type {KnipConfig} from 'knip';
 const config: KnipConfig = {
 	$schema: 'https://unpkg.com/knip@5/schema.json',
 	entry: [],
-	project: ['src/frontend/**/*.ts'],
+	project: ['src/frontend/**/*.{ts,css}'],
 	ignore: [],
-	ignoreDependencies: ['express-static-gzip', 'tailwindcss', 'conventional-changelog-angular', 'chalk', 'slice-ansi', 'string-width'],
+	ignoreFiles: ['src/frontend/style.css'],
+	ignoreDependencies: ['express-static-gzip', 'conventional-changelog-angular', 'chalk', 'slice-ansi', 'string-width'],
 };
 
 export default config;
